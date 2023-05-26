f29f9bd9145cd1be37629ab18fe67c7311b1247a
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-05-25 23:35:19 -0300

    Estrutura condicional Simples
    Primeiros passos com JS:
    Declaração de variáveis, estrutura condicional if/else e interação com usuário: Entrada e Saída de dados.

---
+++ primeiros-passos/condicionais.js
@@ -0,0 +1,33 @@
+/*
+
+Estruturas Condicionais Simples - JS
+
+# Nos ajudam a direcionar o código para sua correta execução, definindo os caminhos possíveis a se seguir.
+# Podemos então definir condições, para que determinado bloco de código seja executado conforme sua necessidade.
+
+//Par ou Impar
+
+const numero = Number(prompt('Informe um valor para verificar se é par ou impar: '))
+const eNumPar = numero % 2 === 0
+
+if(eNumPar){
+    alert('Par')
+} else {
+    alert('Impar')
+}
+
+//Divisivel por 5
+
+const num = Number(prompt('informe um valor para verificar se é divisivel por 5: '))
+const isNumPor5 = (num % 5) === 0
+
+if(isNumPor5){
+    alert('Sim')
+} else {
+    alert('Não')
+}
+
+*/
+
+// Divisivel por 5 usando else if
+
