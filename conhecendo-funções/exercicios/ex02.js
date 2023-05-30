cae177dbc75dc68ca6333988b8d288342aaf4eac
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-05-29 22:45:11 -0300

    Formatação de casas decimais
    Formatação de casas decimais

--- conhecendo-funções/exercicios/ex02.js
+++ conhecendo-funções/exercicios/ex02.js
@@ -18,13 +18,13 @@ function main(){
     let formaPg = Number(prompt('Formas de pagamento: 1- Débito, 2- 2x s/Juros, 3- Pacelamento em até 10x com juros de 10%, 4- Dinheiro ou Pix: '));
 
     if(formaPg === 1){
-        alert(`Compra no débito com 10% de desconto R$: ${aplicarDesconto(precoEtiqueta, 10)}`);
+        alert(`Compra no débito com 10% de desconto R$: ${aplicarDesconto(precoEtiqueta, 10).toFixed(2)}`);
     } else if(formaPg === 2){
         alert(`Compra no cartão em 2x s/Juros R$: ${precoEtiqueta}`);
     } else if(formaPg === 3){
-        alert(`Compra no cartão de crédito em até 10x com 10% de Juros R$: ${aplicarJuros(precoEtiqueta, 10)}`);
+        alert(`Compra no cartão de crédito em até 10x com 10% de Juros R$: ${aplicarJuros(precoEtiqueta, 10).toFixed(2)}`);
     } else if(formaPg === 4){
-        alert(`Compre no dinheiro ou pix com 15% de desconto R$: ${aplicarDesconto(precoEtiqueta, 15)}`);
+        alert(`Compre no dinheiro ou pix com 15% de desconto R$: ${aplicarDesconto(precoEtiqueta, 15).toFixed(2)}`);
     } else {
         alert(`Parâmetro Inválido!`);
     }
