cfb5fbcab0f9c7a06cd207c0de96a2f4cd68017b
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-05-29 21:52:46 -0300

    Estudando funções
    Após a execução dos programas, o trecho de código foi comentado para ficar como lembrete de estudo apenas.

---
+++ conhecendo-funções/introdução/funcoes.js
@@ -0,0 +1,30 @@
+/*  
+
+// Utilizando funções em sua sintaxe básica:
+
+function teste(){
+    console.log('Hello World!');
+}
+teste();
+
+// Declarando função com parâmetro:
+
+function sayMyName(name){
+    console.log("Your is name " + name);
+}
+
+sayMyName('Felipe');
+sayMyName('Kethleen');
+
+// Atribuindo função a uma variável e retornando um valor
+function quadrado(valor){
+    return valor* valor;
+}
+
+const quadradoDez = quadrado(10);
+console.log(quadradoDez);
+
+*/
+
+
+
