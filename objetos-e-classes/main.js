998e701366184df991ed6a96874760a04416517e
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-03 18:29:54 -0300

    Documentando o aprendizado

--- objetos-e-classes/main.js
+++ objetos-e-classes/main.js
@@ -1,3 +1,17 @@
+/* 
+
+# Classes e Objetos:
+
+Um objeto é uma coleção dinâmica de chave e valor:
+    - Em JavaScript podemos atribuiruma função para um objeto, chamamos isso de (método).
+    - Uma vez que declaramos um objeto literal podemos também modificar ele dinamicamente, seja para adicionar valor ou remover.
+
+    O código a seguir trará um exemplo de todos os conceitos estudados até agora.
+
+*/
+
+
+
 const pessoa = {
     nome: prompt('Informe seu nome: '),
     idade: Number(prompt('Informe sua idade: ')),
