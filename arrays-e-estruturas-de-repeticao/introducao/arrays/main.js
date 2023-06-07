e2fe008e3c3ee8874f126e3b3632852179e8639b
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-07 09:01:52 -0300

    Organizando estudos

---
+++ arrays-e-estruturas-de-repeticao/introducao/arrays/main.js
@@ -0,0 +1,27 @@
+/* 
+
+# Arrays:
+
+    - Arryas - São listas sequências, alocadas em memória, podem ter valores iguais ou não.
+    - Para declarar um array, utilizamos a declaração de uma variável normal e passamos os valores dentro de colchetes, exemplo:
+    - alunos = ['Felipe', 'Gabriela', 'Larissa'].
+    - Um array começa sua contagem a paritr da posição 0, ele pode ser iniciado com nenhum atribuição de valor e ser preenchido durante a execução.
+
+*/
+
+// Exemplos de utilização básica de arrays e seus comandos: 
+
+let alunos = ['Felipe', 'Gabriela'];
+alunos.push('Larissa'); // Adiciona um valor no final da lista
+console.log(alunos);
+alunos.pop(); // Remove um valor no final da lista
+console.log(alunos);
+alunos[2] = 'Larissa';
+console.log(alunos); // Adiciona ou subescreve um valor na possição declarada.
+alunos.shift(); // Remove um valor no inicio da lista.
+console.log(alunos);
+
+
+
+
+
