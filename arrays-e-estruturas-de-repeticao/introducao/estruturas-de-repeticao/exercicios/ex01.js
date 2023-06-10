08cacd65612085523f3099bfcebbf6de7c80c38f
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-09 22:34:13 -0300

    Exercícios

--- arrays-e-estruturas-de-repeticao/introducao/estruturas-de-repeticao/exercicios/ex01.js
+++ arrays-e-estruturas-de-repeticao/introducao/estruturas-de-repeticao/exercicios/ex01.js
@@ -1,13 +1,37 @@
 // Calculando média utilizando laços de repetição
 
+/* 
+
+// Solução do curso.
+
 let notas = [5, 7, 8, 5, 8];
 
 let soma = 0;
 
-for (let i = 0; i < notas.length; i++) {
+for (let i = 0; i < notas.length; i++) { 
     const nota = notas[i];
     soma = soma + nota;
 }
 
 let media = soma / notas.length;
-console.log(media.toFixed(2));
+console.log(media.toFixed(2)); 
+
+
+*/
+
+// Minha solução
+
+// Usando interação com usuário básico:
+
+let notas = [];
+
+let soma = 0;
+
+for (let i = 0; i < 3; i++) {
+    notas.push(Number(prompt('Informe as três notas do aluno: ')));
+    const nota = notas[i]
+    soma = soma + nota;
+}
+
+let media = soma / notas.length;
+alert(media.toFixed(2));
---
+++ arrays-e-estruturas-de-repeticao/introducao/estruturas-de-repeticao/exercicios/ex02.js
@@ -0,0 +1,24 @@
+/* 
+
+// Crie um programa que dado determinado número imprima sua tabuada.
+
+// Solução do curso.
+
+for (let index = 1; index < 10; index++) {
+    console.log(index * 5);
+}
+
+*/
+
+// Minha solução 
+
+let num = Number(prompt('Número para calcular tabela: '));
+for (let i = 1; i <= 10; i++) {
+    alert(`${num} x ${i} = ${num * i}`);
+}
+
+
+
+
+
+
