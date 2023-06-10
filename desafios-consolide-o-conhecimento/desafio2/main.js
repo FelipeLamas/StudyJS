c1651c0c9330e428f272b78503979b241f16f026
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-10 19:04:53 -0300

    Avançando com os estudos e exercícios

---
+++ desafios-consolide-o-conhecimento/desafio2/main.js
@@ -0,0 +1,25 @@
+const {gets, print} = require('./funcoes-auxiliares');
+
+const n = gets();
+
+let maiorNumPar = 0;
+let menorNumImpar = 0;
+
+for (let i = 0; i < n; i++) {
+    const numero = gets();
+
+    if(numero % 2 === 0){
+        if(numero > maiorNumPar){
+            maiorNumPar = numero;
+        } 
+    } else {
+        if(menorNumImpar === 0){
+            menorNumImpar = numero;
+        } else if(numero < menorNumImpar){
+            menorNumImpar = numero;
+        }
+    }
+}
+
+print(`O maior número par é ${maiorNumPar}`);
+print(`O menor número impar é ${menorNumImpar}`);
