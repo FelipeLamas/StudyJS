65cf85c1d20c6879cf3927890e7539e57d79db9d
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-11 09:30:12 -0300

    Verificação extra
    Para não permitir notas negativas.

--- desafios-consolide-o-conhecimento/desafio1/main.js
+++ desafios-consolide-o-conhecimento/desafio1/main.js
@@ -11,7 +11,7 @@ const {gets, print} = require('./funcoes-auxiliares');
 
 const media = gets();
 
-if(media < 5){
+if(media >= 0 && media < 5){
     print('Reprovado');
 } else if (media >= 5 && media < 7){
     print('Recuperação');
