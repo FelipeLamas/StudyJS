8989d155eda4f20265a6f24162ddef3d891af37f
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-11 11:03:43 -0300

    Otimizando o código
    Otimizando o código do segundo desafio.

--- desafios-consolide-o-conhecimento/desafio2/main.js
+++ desafios-consolide-o-conhecimento/desafio2/main.js
@@ -8,16 +8,10 @@ let menorNumImpar = 0;
 for (let i = 0; i < n; i++) {
     const numero = gets();
 
-    if(numero % 2 === 0){
-        if(numero > maiorNumPar){
-            maiorNumPar = numero;
-        } 
-    } else {
-        if(menorNumImpar === 0){
+    if(numero % 2 === 0 && numero > maiorNumPar){
+        maiorNumPar = numero;
+    } else if(menorNumImpar === 0 || numero < menorNumImpar){
             menorNumImpar = numero;
-        } else if(numero < menorNumImpar){
-            menorNumImpar = numero;
-        }
     }
 }
 
