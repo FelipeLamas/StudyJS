f7ef2337480887733f21b62df2792be55d6dbdd1
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-12 17:35:42 -0300

    Desafio 3 - Salário + Benefícios - Impostos
    Salário + Benefícios - Impostos de acordo com a faixa salarial.

---
+++ desafios-consolide-o-conhecimento/desafio3/main.js
@@ -0,0 +1,49 @@
+/*  
+    # Faça um programa que calcule e imprima o salário a ser transferido para um funcionário
+    # Para realizar o calculo receba o valor bruto do salário e o adicional dos beneficios
+    # O salário a ser transferido é calculado da seguinte maneira:
+
+    // O valor bruto - percentual de imposto mediante a faixa salarial + adicional dos beneficios
+
+    // Para calcular o percentual de imposto segue as aliquotas:
+
+        - De R$ 0.00 a R$ 1100.00 = 5%
+        - De R$ 1100.01 a R$ 2500.00 = 10%
+        Maior que 2500.00 = 15%
+
+        Exemplo:
+            - Entrada:
+                2000
+                250
+            - Saida:
+                2050.00
+*/
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+const salarioBruto = gets();
+const addBeneficios = gets();
+
+function porcentagem(desconto){
+    return (salarioBruto - (salarioBruto * (desconto / 100)));
+}
+
+function salarioComDescontoImpostoBaseSalarial(){
+    if(salarioBruto >= 0 && salarioBruto <= 1100){
+        return porcentagem(5);
+    } else if(salarioBruto > 1100 && salarioBruto <= 2500){
+        return porcentagem(10);
+    } else if(salarioBruto > 2500){
+        return porcentagem(15);
+    }
+}
+
+function salarioPg(){
+    return salarioComDescontoImpostoBaseSalarial() + addBeneficios;
+}
+
+print(`Salário Liquido: ${salarioComDescontoImpostoBaseSalarial()}`);
+print(`Salário + Beneficios: ${salarioPg()}`);
+
+
+
