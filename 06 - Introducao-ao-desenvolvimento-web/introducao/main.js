e5dcb0671563760aa6674073c6ccc2e596ed2131
Felipe Lamas <100486600+FelipeLamas@users.noreply.github.com>
2023-06-15 09:11:51 -0300

    Introdução ao desenvolvimento web
    HTML, CSS e JavaScript

---
+++ 06 - Introducao-ao-desenvolvimento-web/introducao/main.js
@@ -0,0 +1,11 @@
+/*  
+
+Linguagem de Programação que dá comportamento.
+
+*/
+
+let btn = document.getElementById('mais');
+btn.addEventListener("click", function(){
+    console.log('Você clicou no botão!');
+});
+
